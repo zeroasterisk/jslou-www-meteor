5266cf83b0d02839b0e139fa6e5882bdac1a032c
alan bount <alan@zeroasterisk.com>
2013-10-16 21:09:15 -0400

    hello world a bit nicer

--- client/lib/router.js
+++ client/lib/router.js
@@ -4,6 +4,17 @@
  *   https://github.com/EventedMind/iron-router
  *
  */
+Router.configure({
+  layout: 'layout',
+  notFoundTemplate: 'notFound',
+  loadingTemplate: 'loading',
+  renderTemplates: {
+    /* render the templated named footer to the 'footer' yield */
+    'footer': { to: 'footer' },
+    /* render the template named sidebar to the 'sidebar' yield */
+    'sidebar': { to: 'sidebar' }
+  }
+});
 Router.map(function() {
   this.route('home', {path: '/'});
   this.route('aboutUs');
---
+++ server/config_server.js
@@ -0,0 +1,8 @@
+/**
+ * Server specific config *can* go here
+ *
+ * WARNING: this is a public repo, post no secure information here
+ *
+ * (security through obfuscation = not security)
+ *
+ */
