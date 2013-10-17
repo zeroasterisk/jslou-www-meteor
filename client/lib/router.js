630d0d53e4489f0f4e2f0be6a8c0ccc5d32cbb2a
alan bount <alan@zeroasterisk.com>
2013-10-16 20:48:46 -0400

    hello world - in yo face

---
+++ client/lib/router.js
@@ -0,0 +1,10 @@
+/**
+ * URL Routing
+ *   Handled by Iron Router
+ *   https://github.com/EventedMind/iron-router
+ *
+ */
+Router.map(function() {
+  this.route('home', {path: '/'});
+  this.route('aboutUs');
+});
---
+++ server/app.js
@@ -0,0 +1,8 @@
+/**
+ * Run this on the server, when the server starts
+ *
+ */
+Meteor.startup(function () {
+  // code to run on server at startup
+});
+
